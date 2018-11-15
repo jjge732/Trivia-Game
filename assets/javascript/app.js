e88e4ec51237d8ce8013935b329d6621255893d2
jeast <eastwoodj000@gmail.com>
2018-11-15 09:39:04 -0600

    changed form style to divs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,50 +25,60 @@ let questionSet = [{
     name: 'fiction',
 }]
 
-const insertQuestion = (radio, label, i) => {
-        radio.attr('name', questionSet[count].name);
-        radio.attr('id', `radio${i}`);
-        radio.val(i);
-        label.html(questionSet[count].answerSet[i]);
+// const insertQuestion = (radio, label, i) => {
+//         radio.attr('name', questionSet[count].name);
+//         radio.attr('id', `radio${i}`);
+//         radio.val(i);
+//         label.html(questionSet[count].answerSet[i]);
+// }
+
+// const checkAnswer = () => {
+//     if ($('#radio0').prop('checked', true)) {
+//         correct++;
+//     }
+//     if ($('#radio1').prop('checked', true) || $('#radio2').prop('checked', true) || $('#radio3').prop('checked', true)) {
+//         incorrect++;
+//     }
+//     else {
+//         timeouts++;
+//     }    
+// }
+
+const createMainContent = () => {
+    let answerOption1 = $('<div>').html(questionSet[count].answerSet[0]);
+    let answerOption2 = $('<div>').html(questionSet[count].answerSet[1]);
+    let answerOption3 = $('<div>').html(questionSet[count].answerSet[2]);
+    let answerOption4 = $('<div>').html(questionSet[count].answerSet[3]);
+    $('main').append(answerOption1, answerOption2, answerOption3, answerOption4);
+
 }
 
-const checkAnswer = () => {
-    if ($('#radio0').prop('checked', true)) {
-        correct++;
-    }
-    if ($('#radio1').prop('checked', true) || $('#radio2').prop('checked', true) || $('#radio3').prop('checked', true)) {
-        incorrect++;
-    }
-    else {
-        timeouts++;
-    }    
-}
-
-const createForm = () => {
-    let newDiv = $('<div>');
-    let newButton = $('<button type="submit" form="question">Next Question!</button>');
-    //let newButton = $('<button id="submit">Next Question!</button>');
-    let newForm = $('<form id="question">').html(questionSet[count].question + '<br>');
-    for (let i = 0; i < 4; i++) {
-        let createRadioInput = $('<input>').attr('type', 'radio');
-        let newLabel = $('<label>');
-        insertQuestion(createRadioInput, newLabel, i);
-        newForm.append(createRadioInput);
-        newForm.append(newLabel);
-        newForm.append($('<br>'));
-        newForm.attr('onsubmit', 'checkAnswer()');
-        newDiv.append(newForm);
-    }
-    $('main').html(newDiv);
-    $('main').append(newButton);
-}
+// const createForm = () => {
+//     let newDiv = $('<div>');
+//     let newButton = $('<button type="submit" form="question">Next Question!</button>');
+//     //let newButton = $('<button id="submit">Next Question!</button>');
+//     let newForm = $('<form id="question">').html(questionSet[count].question + '<br>');
+//     for (let i = 0; i < 4; i++) {
+//         let createRadioInput = $('<input>').attr('type', 'radio');
+//         let newLabel = $('<label>');
+//         insertQuestion(createRadioInput, newLabel, i);
+//         newForm.append(createRadioInput);
+//         newForm.append(newLabel);
+//         newForm.append($('<br>'));
+//         newForm.attr('onsubmit', 'checkAnswer()');
+//         newDiv.append(newForm);
+//     }
+//     $('main').html(newDiv);
+//     $('main').append(newButton);
+// }
 
 const nextQuestion = () => {
     // timeouts++; //add timeouts every time; will subtract if submit is pressed
     if (count < questionSet.length) {
         $('main').html('Please wait for the next question to load');
         setTimeout(function() {
-        createForm();
+        //createForm();
+        createMainContent();
         status = setTimeout(nextQuestion, 10000);
         count++;
     }, 3000)
