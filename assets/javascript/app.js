366ab0135d59616206b0742cc591a00f220fbd73
jeast <eastwoodj000@gmail.com>
2018-11-16 12:27:46 -0600

    change style, added randomization to answer order, & made functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,55 +3,30 @@ let correct = 0;
 let incorrect = 0;
 let timeouts = 0;
 let status, masterTime; 
-let prompt = [], answerOption1 = [], answerOption2 = [], answerOption3 = [], answerOption4 = [];
+//let prompt = [], answerOption1 = [], answerOption2 = [], answerOption3 = [], answerOption4 = [];
 let timeKeeper = 0;
 
 //if API
 let categoryNumber = '';
 let queryURL;
 let apiQuestion = [], apiAnswerOption1 = [], apiAnswerOption2 = [], apiAnswerOption3 = [], apiAnswerOption4 = [];
-let categories = ['General Knowledge', 'Books', 'Film', 'Music', 'Musicals & Theatres','Television', 'Video Games', 'Board Games', 'Science & Nature', 'Computers', 'Math', 'Mythology', 'Sports', 'Geography', 'Politics', 'Art', 'Celebrities', 'Animals'];
+let categories = ['General Knowledge', 'Books', 'Film', 'Music', 'Musicals','Television', 'Video Games', 'Board Games', 'Science & Nature', 'Computers', 'Math', 'Mythology', 'Sports', 'Geography', 'Politics', 'Art', 'Celebrities', 'Animals'];
 for (let i = 0; i < categories.length; i++) {
-    let newSpan = $('<span class="categories">').html(categories[i]);
+    let newSpan = $('<span>').html(categories[i]);
     newSpan.attr('data-nbr', (i + 9));
-    $('body').append(newSpan);
+    $('main').append(newSpan);
+    if (i < categories.length / 3) {
+        newSpan.attr('class', 'secondRow categories')
+    }
+    else if (i < categories.length * 2 / 3) {
+        newSpan.attr('class', 'thirdRow categories');
+    }
+    else {
+        newSpan.attr('class', 'fourthRow categories');
+    }
 }
 
-// let questionSet = [{
-//     question: 'What is the capital of Illinois?',
-//     answerSet: [ 'Springfield', 'Chicago', 'Naperville', 'Champaign'],
-//     name: 'cities',
-// },
-// {
-//     question: 'Which president appears on Illinois Licence Plates?',
-//     answerSet: ['Abraham Lincoln', 'John Adams', 'Barack Obama', 'Thomas Jefferson'],
-//     name: 'presidents',
-// },
-// {
-//     question: 'What is the name of the fourth book in the Harry Potter series?',
-//     answerSet: ['The Goblet of Fire', 'The Half-Blood Prince', 'The Order of the Phoenix', 'The Prisoner of Azkaban'],
-//     name: 'books',
-// },
-// {
-//     question: 'What is the name of the protagonist of the Hunger Games Series?',
-//     answerSet: ['Katniss Everdeen', 'Primm Everdeen', 'Peeta Melark', 'Hermoine Grainger'],
-//     name: 'fiction',
-// }]
-
 const createMainContent = () => {
-    // let prompt = $('<h2>').html(questionSet[count].question);
-    // let answerOption1 = $('<div>').html(questionSet[count].answerSet[0]);
-    // answerOption1.attr('class', 'clickable');
-    // answerOption1.attr('data-correct', 'true');
-    // let answerOption2 = $('<div>').html(questionSet[count].answerSet[1]);
-    // answerOption2.attr('class', 'clickable');
-    // answerOption2.attr('data-correct', 'false');
-    // let answerOption3 = $('<div>').html(questionSet[count].answerSet[2]);
-    // answerOption3.attr('class', 'clickable'); 
-    // answerOption3.attr('data-correct', 'false');
-    // let answerOption4 = $('<div>').html(questionSet[count].answerSet[3]);
-    // answerOption4.attr('class', 'clickable');
-    // answerOption4.attr('data-correct', 'false');
     let prompt = $('<h2>').html(apiQuestion[count]);
     let answerOption1 = $('<div>').html(apiAnswerOption1[count]);
     answerOption1.attr('class', 'clickable');
@@ -65,9 +40,30 @@ const createMainContent = () => {
     let answerOption4 = $('<div>').html(apiAnswerOption4[count]);
     answerOption4.attr('class', 'clickable');
     answerOption4.attr('data-correct', 'false');
+    let random = Math.floor(Math.random() * 4);
     $('main').html(prompt);
-    $('main').append(answerOption1, answerOption2, answerOption3, answerOption4);
-    $('body').prepend('<h3>');
+    for (let j = 0; j < 4; j++) {
+        switch (random) {
+            case 0:
+                $('main').append(answerOption1);
+                random++;
+                break;
+            case 1:
+                $('main').append(answerOption2);
+                random++;
+                break;
+            case 2:
+                $('main').append(answerOption3);
+                random++;
+                break;
+            case 3:
+                $('main').append(answerOption4);
+                random = 0;
+                break;
+        }
+
+    }
+    $('header').prepend('<h3>');
     timeKeeper = 1000;
     masterTime = setInterval(function() {
             timeKeeper--;
@@ -86,13 +82,13 @@ const nextQuestion = (accuracy = -1) => {
         }
         else if (count > 0) {
             if (accuracy === -1) {
-                $('main').html(`<h2>The correct answer was ${apiAnswerOption1[count - 1].apiAnswerOption1[count]}. Try to answer more quickly!</h2>`);
+                $('main').html(`<h2>The correct answer was ${apiAnswerOption1[count - 1]}. Try to answer more quickly!</h2>`);
             }
             else if (accuracy) {
                 $('main').html('<h2>Correct!</h2>');
             }
             else if (!accuracy) {
-                $('main').html(`<h2>Incorrect! The correct answer was ${apiAnswerOption1[count - 1].apiAnswerOption1[count]}.</h2>`);
+                $('main').html(`<h2>Incorrect! The correct answer was ${apiAnswerOption1[count - 1]}.</h2>`);
             }
         }
         setTimeout(function() {
@@ -103,13 +99,13 @@ const nextQuestion = (accuracy = -1) => {
     }
     else if (count === apiQuestion.length) {
         if (accuracy === -1) {
-            $('main').html(`<h2>The correct answer was ${apiAnswerOption1[count - 1].apiAnswerOption1[0]}. Try to answer more quickly!</h2>`);
+            $('main').html(`<h2>The correct answer was ${apiAnswerOption1[count - 1]}. Try to answer more quickly!</h2>`);
         }
         else if (accuracy) {
             $('main').html('<h2>Correct!</h2>');
         }
         else if (!accuracy) {
-            $('main').html(`<h2>Incorrect! The correct answer was ${apiAnswerOption1[count - 1].apiAnswerOption1[0]}.</h2>`);
+            $('main').html(`<h2>Incorrect! The correct answer was ${apiAnswerOption1[count - 1]}.</h2>`);
         }
         setTimeout(nextQuestion, 3000);
         count++;
@@ -144,6 +140,7 @@ const timeConverter = t => {
 $(document).on('click', '.categories', function() {
     categoryNumber = $(this).attr('data-nbr');
     queryURL = `https://opentdb.com/api.php?amount=10&category=${categoryNumber}&type=multiple`;
+    $('body').prepend($('<header>'));
     $.ajax({
         url: queryURL,
         method: 'GET'
@@ -160,16 +157,7 @@ $(document).on('click', '.categories', function() {
     })
 })
 
-// $(document).on('click', '#submit', function() {
-//     clearTimeout(status);
-//     // setInterval(createHeader, 10);
-//     nextQuestion();
-//     // headerTime = $('body').prepend($('<header>'));
-// })
-
 $(document).on('click', '.clickable', function() {
-    clearTimeout(status);
-    nextQuestion();
     clearTimeout(status);
     let accurate;
     if ($(this).attr('data-correct') === 'true') {
